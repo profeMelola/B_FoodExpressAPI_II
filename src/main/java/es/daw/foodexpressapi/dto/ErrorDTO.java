5bd0bf2af7426405bcf3a2b356b43a77884f9f89
melola <melola@educa.madrid.org>
2026-01-13 18:00:19 +0100

    meter errorDTO en gestión de excepciones

--- src/main/java/es/daw/foodexpressapi/dto/ErrorDTO.java
+++ src/main/java/es/daw/foodexpressapi/dto/ErrorDTO.java
@@ -1,5 +1,6 @@
 package es.daw.foodexpressapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,7 +8,9 @@ import java.time.LocalDateTime;
 @Data
 public class ErrorDTO {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp; // Momento del error
+
     private int status;              // Código HTTP (404, 400, 500...)
     private String error;            // Nombre del error: "Not Found", "Bad Request"...
     private String message;          // Mensaje detallado
--- src/main/java/es/daw/foodexpressapi/exception/GlobalExceptionHandler.java
+++ src/main/java/es/daw/foodexpressapi/exception/GlobalExceptionHandler.java
@@ -1,31 +1,71 @@
 package es.daw.foodexpressapi.exception;
 
+import es.daw.foodexpressapi.dto.ErrorDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.http.parser.HttpHeaderParser;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+//    @ExceptionHandler(InvalidStatusException.class)
+//    public ResponseEntity<String> handleInvalidStatus(InvalidStatusException ex) {
+//        return ResponseEntity
+//                .status(HttpStatus.BAD_REQUEST)
+//                .body(ex.getMessage());
+//    }
+//
+//    @ExceptionHandler(UserNotFoundException.class)
+//    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
+//        return ResponseEntity
+//                .status(HttpStatus.NOT_FOUND)
+//                .body(ex.getMessage());
+//    }
+//
+//    @ExceptionHandler(RestaurantNotFoundException.class)
+//    public ResponseEntity<String> handleRestaurantNotFound(RestaurantNotFoundException ex) {
+//        return ResponseEntity
+//                .status(HttpStatus.NOT_FOUND)
+//                .body(ex.getMessage());
+//    }
     @ExceptionHandler(InvalidStatusException.class)
-    public ResponseEntity<String> handleInvalidStatus(InvalidStatusException ex) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(ex.getMessage());
+    public ResponseEntity<ErrorDTO> handleInvalidStatus(InvalidStatusException ex,  HttpServletRequest request) {
+        return buildError(HttpStatus.BAD_REQUEST, ex.getMessage(), request);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+    public ResponseEntity<ErrorDTO> handleUserNotFound(UserNotFoundException ex, HttpServletRequest request) {
+        return buildError(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
 
     @ExceptionHandler(RestaurantNotFoundException.class)
-    public ResponseEntity<String> handleRestaurantNotFound(RestaurantNotFoundException ex) {
-        return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+    public ResponseEntity<ErrorDTO> handleRestaurantNotFound(RestaurantNotFoundException ex,  HttpServletRequest request) {
+        return buildError(HttpStatus.NOT_FOUND, ex.getMessage(), request);
     }
+
+    /**
+     *     private LocalDateTime timestamp; // Momento del error
+     *
+     *     private int status;              // Código HTTP (404, 400, 500...)
+     *     private String error;            // Nombre del error: "Not Found", "Bad Request"...
+     *     private String message;          // Mensaje detallado
+     *     private String path;             // Endpoint que falló (/api/dishes, etc.)
+     * @return
+     */
+    private ResponseEntity<ErrorDTO> buildError(HttpStatus status, String message, HttpServletRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO();
+        errorDTO.setStatus(status.value()); // Código HTTP (404, 400, 500...)
+        errorDTO.setError(status.getReasonPhrase()); // Nombre del error: "Not Found", "Bad Request"...
+        errorDTO.setMessage(message);
+        errorDTO.setTimestamp(LocalDateTime.now()); // no formateamos... lo dejamos en dto
+        errorDTO.setPath(request.getRequestURI()); // Endpoint que falló (/api/dishes, etc.)
+        return ResponseEntity.status(status).body(errorDTO);
+
+    }
+
 }
